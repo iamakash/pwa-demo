c9780ba290d90a90fed9d948072d01cc0591f2e3
iamAkash <akash@nexthoughts.com>
2017-07-26 13:47:56 +0530

    Step Three

--- basic-example/scripts/app.js
+++ basic-example/scripts/app.js
@@ -60,7 +60,7 @@
         var label = selected.textContent;
         app.getForecast(key, label);
         app.selectedCities.push({key: key, label: label});
-        // app.saveSelectedCities();
+        app.saveSelectedCities();
         app.toggleAddDialog(false);
     });
 
@@ -157,5 +157,25 @@
         });
     };
 
-    app.updateForecastCard(injectedForecast);
+    app.saveSelectedCities = function () {
+        window.localforage.setItem('selectedCities', app.selectedCities);
+    };
+
+    document.addEventListener('DOMContentLoaded', function () {
+        window.localforage.getItem('selectedCities', function (err, cityList) {
+            if (cityList) {
+                app.selectedCities = cityList;
+                app.selectedCities.forEach(function (city) {
+                    app.getForecast(city.key, city.label);
+                });
+            } else {
+                app.updateForecastCard(injectedForecast);
+                app.selectedCities = [
+                    {key: injectedForecast.key, label: injectedForecast.label}
+                ];
+                app.saveSelectedCities();
+            }
+        });
+    });
+
 })();
