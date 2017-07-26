b4286e40a205ff825473cfafb4175e21cda0e9fe
iamAkash <akash@nexthoughts.com>
2017-07-26 14:55:59 +0530

    Step Four

--- basic-example/scripts/app.js
+++ basic-example/scripts/app.js
@@ -178,4 +178,11 @@
         });
     });
 
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then(function () {
+                console.log('Service Worker Registered');
+            });
+    }
 })();
